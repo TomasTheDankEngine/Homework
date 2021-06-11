3afdc0c3b5c2a679bf7074b900120627e9db46df
TomasTheDankEngine <tomas.miri@gmail.com>
2021-06-11 15:04:52 +0300

    hw 7

--- 7/7-5/red.php
+++ 7/7-5/red.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Orange</title>
+    <title>Document</title>
 </head>
 <body>
-    <a href="http://localhost/1-php/Homework/7/7-4/lemon.php">To lemon</a>
+    <a href="./red.php?pageSet=true">red</a>
 </body>
 </html>
-<style>
-    body {
-        background-color: #ffb30f;
-    }
-</style>
+<?php
+if (isset($_GET['pageSet'])) {
+    header('Location: ./blue.php');
+    die;
+}
