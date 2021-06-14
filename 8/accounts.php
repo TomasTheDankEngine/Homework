42f30e00c74e74aeada2b05ea52cfeea7a4a795c
TomasTheDankEngine <tomas.miri@gmail.com>
2021-06-14 13:42:36 +0300

    8 init commit

---
+++ 8/accounts.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account overview</title>
+</head>
+
+<body>
+    <!-- Accounts list -->
+    <h2>Accounts list</h2>
+    <div>
+
+    </div>
+</body>
+
+</html>
---
+++ 8/newacc.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New account</title>
+</head>
+
+<body>
+    <!-- New account creation -->
+    <h2>Create new account</h2>
+    <form action="submit">
+        <div>
+            <span>Name</span>
+            <span>Surname</span>
+            <span>Account number</span>
+            <span>Personal ID number</span>
+        </div>
+        <div>
+        <button>Create account</button>
+        </div>
+    </form>
+</body>
+
+</html>
